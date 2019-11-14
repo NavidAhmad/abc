ca1d3bd05d00cdd881956645afb126ab40026f2f
NavidAhmad <toengnrnaveed@gmail.com>
2019-11-14 15:35:57 +0500

    Updates

--- src/app/shared/explore-hcn/explore-hcn-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ExploreHCNComponent } from './explore-hcn.component';
-import { CommonModule } from '@angular/common';
-
-
-@NgModule({}
-  )
-export class ExploreHCNRoutingModule { }
